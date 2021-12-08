02681643d2073d358b7f8b251f4d0206a2a159a1
DEVA <devidas.labade@gmail.com>
2021-12-08 22:48:09 +0530

    added encrypt decrypt functionality

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import json
 import datetime
 import traceback
 from configparser import ConfigParser
-import rsa
+from cryptography.fernet import Fernet
+
+
 # instantiate config Parser
+
 config = ConfigParser()
 
 def main(configPath):
     try:
+        #key = Fernet.generate_key()
+        #print(key)
+        key = b'jhj1m0i98bMghl4jCk57hmMuW4fdvFv--S1SSmsCijE='
+        cipher_suite = Fernet(key)
+        ciphered_text = cipher_suite.encrypt(b"SuperSecretPassword")  # required to be bytes
+        print(ciphered_text)
+
+        ciphered_text = b'gAAAAABhsOdnGpEEm0fcdUO7cDdmwhnvW-q99Nm8-xY1-bsQBljnS9QZWqzPzJOr7oMWhUkd3j8XlIPMWhqCmH_aRQbqPiTwcFCf0rsTYNSQ1Pyty8IELc4='
+        unciphered_text = (cipher_suite.decrypt(ciphered_text))
+        print(unciphered_text.decode("utf-8"))
         config.read(configPath)
         print("Starting kafka consumer with following details::")
         host = config['mysql']['host']
@@ -35,36 +48,8 @@ def main(configPath):
         print(f'Password: {passwd2}')
         print(f'Database: {db2}')
 
-        # generate public and private keys with
-        # rsa.newkeys method,this method accepts
-        # key length as its parameter
-        # key length should be atleast 16
-        publicKey, privateKey = rsa.newkeys(512)
-        print("private key:",privateKey)
-        print("public key:" , publicKey)
 
-        # this is the string that we will be encrypting
-        message = "deva@123"
 
-        # rsa.encrypt method is used to encrypt
-        # string with public key string should be
-        # encode to byte string before encryption
-        # with encode method
-        encMessage = rsa.encrypt(message.encode(),
-                                 publicKey)
-
-        print("original string: ", message)
-        print("encrypted string: ", encMessage)
-
-        # the encrypted message can be decrypted
-        # with ras.decrypt method and private key
-        # decrypt method returns encoded byte string,
-        # use decode method to convert it to string
-        # public key cannot be used for decryption
-
-        decMessage = rsa.decrypt(encMessage, privateKey).decode()
-
-        print("decrypted string: ", decMessage)
     except Exception as e:
         print(str(datetime.datetime.now()) + "____________ Exception occurred in main() ________________")
         print("Exception::msg %s" % str(e))
